609770a712c3c4690d575bea6eac44896cdbeb92
Michele Maffei <michele.muffin@gmail.com>
2015-10-13 14:52:56 +0200

    Initial Commit

---
+++ EnNeMonitor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EnNeMonitor
+//
+//  Created by Michele Maffei on 13/10/15.
+//  Copyright © 2015 Michele Maffei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EnNeMonitor/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EnNeMonitor
+//
+//  Created by Michele Maffei on 13/10/15.
+//  Copyright © 2015 Michele Maffei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
